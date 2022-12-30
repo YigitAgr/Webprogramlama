ee2b72a635b309224de3e806806573666d006027
Yigit Agar <yigit.agar22@gmail.com>
2022-12-30 23:27:44 +0300

    ver,tabanı ekleme formu bir veri eklenedi

--- Controllers/ClubController.cs
+++ Controllers/ClubController.cs
@@ -28,5 +28,16 @@ namespace Webprogramlama.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Club club) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View(club);
+            }
+            _clubRepository.Add(club);
+            return RedirectToAction("index");
+        }
     }
 }
